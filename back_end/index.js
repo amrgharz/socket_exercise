fabd69e13067e62996610621fed6a7aebbb51028
amrgharz <ammor.ghr@gmail.com>
2018-05-07 11:05:09 +0300

    first commit

---
+++ back_end/index.js
@@ -0,0 +1,12 @@
+const http = require ('http')
+
+const server = http.createServer()
+
+const handleRequest = (req , res) =>{
+    res.end('ok!')
+}
+
+server.on('request' , handleRequest)
+
+server.listen(8888 , ()=> console.log('server is ready'))
+
